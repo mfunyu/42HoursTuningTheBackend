6b65f2198dc182e615dcc76b02a54c1a5e64921f
mfunyu <mfunyu@student.42tokyo.jp>
2022-04-17 07:57:23 +0900

    SQL: INDEX slow-sql用に追加

--- development/mysql/sql/V1.sql
+++ development/mysql/sql/V1.sql
@@ -3,9 +3,9 @@ ALTER TABLE group_member ADD INDEX user_id_index(user_id);
 
 ALTER TABLE record_last_access ADD INDEX user_id_index(user_id);
 
--- この２つは順番大切
-ALTER TABLE record ADD INDEX created_by_index(created_by);
-ALTER TABLE record ADD INDEX status_index(status);
+-- 
+ALTER TABLE record ADD INDEX created_by_index(created_by, status);
+alter table record add index test_index(updated_at, record_id desc, status);
 
 -- この２つは順番大切
 ALTER TABLE record_comment ADD INDEX linked_record_id_index(linked_record_id);
