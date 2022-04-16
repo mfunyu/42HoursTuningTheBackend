1c71a6a42be89fce939bc93d86c21805c7856ddc
mfunyu <mfunyu@student.42tokyo.jp>
2022-04-16 14:12:30 +0900

    SQL: ADD INDEXを２つ追加

--- development/mysql/sql/V0.sql
+++ development/mysql/sql/V0.sql
@@ -84,3 +84,6 @@ CREATE TABLE `file` (
     `name` VARCHAR(256) NOT NULL,
     PRIMARY KEY (`file_id`)
 );
+
+ALTER TABLE session ADD INDEX value_index(value);
+ALTER TABLE group_member ADD INDEX user_id_index(user_id);
