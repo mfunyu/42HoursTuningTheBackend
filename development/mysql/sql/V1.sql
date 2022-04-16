2754b4fb694f322b1f7d50d533aec3d8d2d42fdf
mfunyu <mfunyu@student.42tokyo.jp>
2022-04-16 16:20:14 +0900

    SQL: V1ファイルINDEX追加

--- development/mysql/sql/V1.sql
+++ development/mysql/sql/V1.sql
@@ -1,2 +1,12 @@
 ALTER TABLE session ADD INDEX value_index(value);
 ALTER TABLE group_member ADD INDEX user_id_index(user_id);
+
+ALTER TABLE record_last_access ADD INDEX user_id_index(user_id);
+
+-- この２つは順番大切
+ALTER TABLE record ADD INDEX created_by_index(created_by);
+ALTER TABLE record ADD INDEX status_index(status);
+
+-- この２つは順番大切
+ALTER TABLE record_comment ADD INDEX linked_record_id_index(linked_record_id);
+ALTER TABLE record_comment ADD INDEX created_at_index(created_at);
