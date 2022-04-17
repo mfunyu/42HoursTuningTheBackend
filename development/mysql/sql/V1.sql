7ed0060086a641dd0206aefaeb9fa92268a9f0cd
mfunyu <mfunyu@student.42tokyo.jp>
2022-04-17 11:26:08 +0900

    SQL: pt-query-digest結果からindex追加

--- development/mysql/sql/V1.sql
+++ development/mysql/sql/V1.sql
@@ -6,7 +6,10 @@ ALTER TABLE record_last_access ADD INDEX user_id_index(user_id);
 -- 
 ALTER TABLE record ADD INDEX created_by_index(created_by, status);
 alter table record add index test_index(updated_at, record_id desc, status);
+alter table record add index category_pair_index(category_id, application_group);
 
 -- この２つは順番大切
 ALTER TABLE record_comment ADD INDEX linked_record_id_index(linked_record_id);
 ALTER TABLE record_comment ADD INDEX created_at_index(created_at);
+
+CREATE INDEX add_index ON record_item_file (linked_record_id); 
